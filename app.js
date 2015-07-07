99fad56e7ef3d20e56bb9ca67ba341d4d875f38f
NoNameA 774 <nonamea.774@gmail.com>
2015-07-07 23:11:39 +0900

    codel の読み込みまで出来た。

---
+++ app.js
@@ -0,0 +1,51 @@
+// app.js
+
+var Canvas = require('canvas')
+  , Image = Canvas.Image
+  , config = require('./config')
+  , fs = require('fs');
+
+function main(image) {
+    var canvas = new Canvas(config.width * config.codel, config.height * config.codel);
+    var ctx = canvas.getContext('2d');
+
+    ctx.drawImage(image, 0, 0);
+
+    var code = new Array(config.height);
+    for(var i = 0; i < config.height; ++i) {
+	code[i] = new Array(config.width);
+    }
+    // 確かに効率は悪い、しかし、それが問題となるほどの大きさのPietを描けるのでしょうか(余白作ればいいだけだから描けそうだ)。
+    for(i = 0; i < config.height; ++i) {
+	for(var j = 0; j < config.width; ++j) {
+	    var color = pick_color(ctx, j * config.codel, i * config.codel);
+	    for (var k in config.colors) {
+		if (color === config.colors[k]) {
+		    code[i][j] = k;
+		    break;
+		}
+	    }
+	}
+    }
+
+    for(i = 0; i < config.height; ++i) {
+	for(var j = 0; j < config.width; ++j) {
+	    process.stdout.write(code[i][j]);
+	    process.stdout.write(',');
+	}
+	process.stdout.write("\n");
+    }
+}
+
+function pick_color(ctx, x, y) {
+    var img = ctx.getImageData(x, y, 1, 1);
+    var data = img.data;
+    return (data[0] << 16) + (data[1] << 8) + (data[2] << 0);
+}
+
+fs.readFile(config.filename, function(err, data) {
+    if (err) throw err;
+    image = new Image();
+    image.src = data;
+    main(image);
+});
---
+++ config.js
@@ -0,0 +1,48 @@
+// config.js
+
+var codel = 10;
+var width = 20;
+var height = 80;
+var filename = 'image.png';
+
+if (width % codel != 0 || height % codel != 0) {
+    console.error("yabasou");
+}
+
+var colors = {
+    'lred': 0xffc0c0, // light red
+    'lyellow': 0xffffc0, // light yellow
+    'lgreen': 0xc0ffc0, // light green
+    'lcyan': 0xc0ffff, // light cyan
+    'lblue': 0xc0c0ff, // light blue
+    'lmagenta': 0xffc0ff, // light magenta
+
+    'red': 0xff0000, // red
+    'yellow': 0xffff00, // yellow
+    'green': 0x00ff00, // green
+    'cyan': 0x00ffff, // cyan
+    'blue': 0x0000ff, // blue
+    'magenta': 0xff00ff, // magenta
+
+    'dred': 0xc00000, // dark red
+    'dyellow': 0xc0c000, // dark yellow
+    'dgreen': 0x00c000, // dark green
+    'dcyan': 0x00c0c0, // dark cyan
+    'dblue': 0x0000c0, // dark blue
+    'dmagenta': 0xc000c0, // dark magenta
+
+    'white': 0xffffff, // white
+    'black': 0x000000, // black
+};
+
+// rewrite here
+var config  = module.exports = {
+    codel: codel,
+    width: width / codel,
+    height: height / codel,
+    filename: filename,
+
+    colors: colors,
+};
+
+
