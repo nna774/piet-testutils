ac8846ae1cfe97fb320e18498467f55629241da9
NoNameA 774 <nonamea774@gmail.com>
2016-09-06 04:17:49 +0900

    :green_heart:

--- utils.js
+++ utils.js
@@ -1,15 +1,14 @@
-function randomTest(randgen, teacher, count) {
-  count = count || 100;
-  var cases = [];
-  for (var i = 0; i < count; ++i) {
-    var rand1 = randgen();
-    var rand2 = randgen();
-    var ans1 = teacher(rand1, rand2);
-    var ans2 = teacher(rand2, rand1);
-    
+function randomTest(randgen, teacher, count = 100) {
+  const cases = [];
+  for (let i = 0; i < count; ++i) {
+    const rand1 = randgen();
+    const rand2 = randgen();
+    const ans1 = teacher(rand1, rand2);
+    const ans2 = teacher(rand2, rand1);
+
     cases.push(
       {
-        name: 'rand ' + String(rand1) + ' ' + String(rand2),
+        name: `rand ${String(rand1)} ${String(rand2)}`,
         desc: 'rand',
         input: [String(rand1), String(rand2)],
         expect: String(ans1),
@@ -17,7 +16,7 @@ function randomTest(randgen, teacher, count) {
     );
     cases.push(
       {
-        name: 'rand ' + String(rand2) + ' ' + String(rand1),
+        name: `rand ${String(rand2)} ${String(rand1)}`,
         desc: 'rand',
         input: [String(rand2), String(rand1)],
         expect: String(ans2),
@@ -28,5 +27,5 @@ function randomTest(randgen, teacher, count) {
 }
 
 module.exports = {
-  randomTest: randomTest,
+  randomTest,
 };
