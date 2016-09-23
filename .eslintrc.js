7cc46c029f06c4b7c04ecbe3933c80b6556f1a1a
NoNameA 774 <nonamea774@gmail.com>
2016-09-23 17:50:15 +0900

    disable no-plusplus and no-bitwise

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   "extends": "airbnb",
   "rules": {
     "no-console": "off",
+    "no-bitwise": "off",
+    "no-plusplus": "off",
     "no-mixed-operators": ["error", { "allowSamePrecedence": true }],
     "yoda": ["error", "never", { "exceptRange": true }],
     "strict": ["error", "function"],
